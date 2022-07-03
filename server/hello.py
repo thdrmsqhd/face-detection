37e2e31b1abada89870f64e177df16982bc88d76
thdrmsqhd <thdrmsqhd@googmail.com>
2022-07-03 15:50:06 +0900

    add analysis

--- server/hello.py
+++ server/hello.py
@@ -1,1 +1,14 @@
 from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    # 여기에 안면인식 정보를 받아들인다.
+    # 받아들인 안면인식 정보를 기반으로 감정을 분석한다.
+    # 분석한 감정을 응답해준다
+    # sad, happy, angry
+    return {'화남': '30%'}
+
+if __name__ == '__main__':
+    app.run()
